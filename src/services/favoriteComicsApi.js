d16380ce28738f26251a46085c4e62f346ea9341
Pedro Rambo <phhrambo@gmail.com>
2022-05-09 21:59:50 -0300

    Renamed REACT_APP_PERSISTENCE_URL environment variable to REACT_APP_FAVORITE_COMICS_API_URL

--- src/services/favoriteComicsApi.js
+++ src/services/favoriteComicsApi.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const {
-  REACT_APP_PERSISTENCE_URL
+  REACT_APP_FAVORITE_COMICS_API_URL
 } = process.env
 
 const api = axios.create({
-  baseURL: REACT_APP_PERSISTENCE_URL
+  baseURL: REACT_APP_FAVORITE_COMICS_API_URL
 })
 
 api.interceptors.request.use(request => {
