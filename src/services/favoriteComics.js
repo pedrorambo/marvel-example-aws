bbf9f0e3d21a492098cdf4c479160057c6bed957
Pedro Rambo <phhrambo@gmail.com>
2022-05-09 22:07:59 -0300

    Fixed documentation on favoriteComics service

--- src/services/favoriteComics.js
+++ src/services/favoriteComics.js
@@ -2,8 +2,8 @@ import api from './favoriteComicsApi'
 
 /**
  * 
- * @param {string} params.userId User Id
- * @return {string[]} Ids of the favorite comics
+ * @param {{userId: string}} params
+ * @return {Promise<string[]>} Ids of the favorite comics
  */
 export const getFavoriteComicsService = async params => {
   const { data: response } = await api.get('/', { params })
@@ -12,8 +12,8 @@ export const getFavoriteComicsService = async params => {
 
 /**
  * 
- * @param {string} params.userId User Id 
- * @param {string} params.comicId Comic Id 
+ * @param {{userId: string, comicId: string}} params
+ * @return {Promise<void>}
  */
 export const setFavoriteComicService = async params => {
   const { data: response } = await api.post('/', { params })
@@ -22,8 +22,8 @@ export const setFavoriteComicService = async params => {
 
 /**
  * 
- * @param {string} params.userId User Id 
- * @param {string} params.comicId Comic Id 
+ * @param {{userId: string, comicId: string}} params
+ * @return {Promise<void>}
  */
 export const deleteFavoriteComicService = async params => {
   const { data: response } = await api.delete("/", { params })
